2cf2d1cfd209f3569afbacd9a3965a258126ed32
Nicolas Mokus <nmks@tuta.io>
2024-02-20 11:20:10 -0500

    MAINT: add __main__ file to run package as module
    * extract main() to own file
    * extract argparse logic to this file

---
+++ src/tide_gatherer/__main__.py
@@ -0,0 +1,51 @@
+# /usr/bin/env python3
+
+"""Collect tide information from marees.gc.ca"""
+
+import argparse
+import pathlib
+
+
+def main(args): ...
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("data_path", type=pathlib.Path)
+    parser.add_argument(
+        "--year",
+        type=int,
+        nargs="?",
+        default=2024,
+        help="Year",
+    )
+    mode = parser.add_argument_group("Mode", "TODO desc")
+    exclusive_group = mode.add_mutually_exclusive_group(required=True)
+    exclusive_group.add_argument("--discover", help="TODO")
+    exclusive_group.add_argument("--target", type=str, nargs=1, help="TODO")
+    parser.add_argument(
+        "--interactive",
+        action="store_true",
+        help="TODO",
+    )
+    parser.add_argument(
+        "--verbose",
+        action="store_true",
+        help="TODO",
+    )
+    parser.add_argument(
+        "--resolution",
+        type=int,
+        nargs="?",
+        default=1,
+        help="TODO",
+    )
+    parser.add_argument(
+        "--date",
+        type=int,
+        nargs="?",
+        default=1,
+        help="TODO",
+    )
+    args = parser.parse_args()
+    main(args)
--- src/tide_gatherer/tide_gatherer.py
+++ src/tide_gatherer/tide_gatherer.py
@@ -2,7 +2,6 @@
 
 """Collect tide information from marees.gc.ca"""
 
-import argparse
 import enum
 import datetime
 import pathlib
@@ -96,49 +95,3 @@ def discover(path: pathlib.Path):
 
             df = make_df(year, month, day, timezone, resolution)
             filename = make_filename
-
-
-def main(args):
-    df = make_df()
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("data_path", type=pathlib.Path)
-    parser.add_argument(
-        "--year",
-        type=int,
-        nargs="?",
-        default=2024,
-        help="Year",
-    )
-    mode = parser.add_argument_group('Mode', 'TODO desc')
-    exclusive_group = mode.add_mutually_exclusive_group(required=True)
-    exclusive_group.add_argument('--discover', help='TODO')
-    exclusive_group.add_argument('--target', type=str, nargs=1, help="TODO")
-    parser.add_argument(
-        "--interactive",
-        action="store_true",
-        help="TODO",
-    )
-    parser.add_argument(
-        "--verbose",
-        action="store_true",
-        help="TODO",
-    )
-    parser.add_argument(
-        "--resolution",
-        type=int,
-        nargs="?",
-        default=1,
-        help="TODO",
-    )
-    parser.add_argument(
-        "--date",
-        type=int,
-        nargs="?",
-        default=1,
-        help="TODO",
-    )
-    args = parser.parse_args()
-    main(args)
