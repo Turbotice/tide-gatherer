396f77e595240fd93423224ab0ff16cd9cb99ccf
Nicolas Mokus <nmks@tuta.io>
2024-02-22 15:53:00 -0500

    MAINT: implement verbose flag

--- src/tide_gatherer/tide_gatherer.py
+++ src/tide_gatherer/tide_gatherer.py
@@ -8,7 +8,7 @@ import pathlib
 import polars as pl
 import pytz
 import requests
-from typing import Iterator
+from typing import Callable, Iterator
 
 
 class Resolution(enum.Enum):
@@ -27,12 +27,6 @@ tz_str = "America/Montreal"
 url = server_url + f"/api/v1/stations/{rimouski_id}/data"
 
 
-# TODO: make into a proper decorator preventing FS side effects
-def dry_run(fun, kw):
-    if not kw:
-        return fun
-
-
 def work(
     year: int,
     resolution: Resolution,
@@ -44,10 +38,10 @@ def work(
         month, day = str_to_date(stem)
         start_time, end_time = date_to_iso(year, month, day, tz_str)
 
-        json = get_json(start_time, end_time, resolution)
+        json = get_json(start_time, end_time, resolution, **kwargs)
         df = make_df(json, tz_str)
 
-        filename = make_filename(start_time, resolution)
+        filename = make_filename(start_time, resolution, **kwargs)
         path = _path.joinpath("Marees")
         make_path(path, **kwargs)
         filepath = path.joinpath(filename)
@@ -55,25 +49,32 @@ def work(
 
 
 def make_path(path: pathlib.Path, **kwargs):
+    if kwargs["verbose"]:
+        print(f"Creating directory {path}")
     if not kwargs["dry_run"]:
         if not path.exists():
             path.mkdir(parents=True)
 
 
-def str_to_date(date: str) -> Iterator[tuple[int, int]]:  # TODO: return type
+def str_to_date(date: str) -> Iterator[tuple[int, int]]:
     """Turn a mmdd string into a pair of mm, dd ints"""
     return map(int, (date[:2], date[2:]))
 
 
 def get_json(
-    start_time: str, end_time: str, resolution: Resolution
-) -> list[dict[str, str | float]]:
+    start_time: str, end_time: str, resolution: Resolution, **kwargs
+) -> list[dict[str, str | float | bool]]:
     payload = {
         "time-series-code": data_code,
         "resolution": resolution.name,
         "from": start_time,
         "to": end_time,
     }
+    if kwargs["verbose"]:
+        print(
+            f"Sending request for tide data, from {payload['from']} to {payload['to']}, "
+            f"with resolution {resolution.value} minutes"
+        )
 
     r = requests.get(url, params=payload)
     if not r.status_code == 200:
@@ -81,7 +82,11 @@ def get_json(
     return r.json()
 
 
-def make_df(json: list[dict[str, str | float]], timezone: str) -> pl.DataFrame:
+def make_df(
+    json_content: list[dict[str, str | float | bool]], timezone: str, **kwargs
+) -> pl.DataFrame:
+    if kwargs["verbose"]:
+        print("Building dataframe from tide data")
     tz = pytz.timezone(timezone)
     df = pl.from_dict(
         dict(
@@ -93,7 +98,7 @@ def make_df(json: list[dict[str, str | float]], timezone: str) -> pl.DataFrame:
                             _d["eventDate"],
                             _d["value"] if _d["reviewed"] else None,
                         ),
-                        json,
+                        json_content,
                     )
                 ),
             )
@@ -109,7 +114,6 @@ def make_filename(dfrom: str, resolution: Resolution) -> str:
 
 def date_to_iso(year: int, month: int, day: int, timezone: str) -> list[str]:
     tz = pytz.timezone(timezone)
-
     return [
         tz.localize(
             datetime.datetime.strptime(f"{year}-{month}-{day} {time}", "%Y-%m-%d %H:%M")
@@ -119,7 +123,7 @@ def date_to_iso(year: int, month: int, day: int, timezone: str) -> list[str]:
 
 
 def write_file(df: pl.DataFrame, path: pathlib.Path, **kwargs):
+    if kwargs["verbose"]:
+        print(f"Writing dataframe to {path}")
     if not kwargs["dry_run"]:
         df.write_csv(path)
-    else:
-        print(f"Writing df to {path}")
